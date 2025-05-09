560cb7a4d567a0915c952f3aecb6b4e43456a1cd
Leopoldo López <leolopezdealba94@gmail.com>
2025-05-09 12:10:24 -0600

    add new Banner and Amenities sections to the Typology page

--- src/components/Typologies.tsx
+++ src/components/Typologies.tsx
@@ -3,7 +3,7 @@ import TypologyElement from './TypologyElement';
 
 const Typologies = ({ typologies }: { typologies: Array<any> }) => {
     return (
-        <div className="relative top-[-200px]">
+        <div className="top-[-200px]">
             <div className="bg-secondary m-[20px] sm:grid-cols-1">
                 <div className="grid grid-cols-1 md:grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-3">
                     {typologies.map((typology) => (
--- src/pages/Typologies.tsx
+++ src/pages/Typologies.tsx
@@ -3,12 +3,16 @@ import Typologies from '../components/Typologies';
 import HeroSection from '../components/ImageHeroSection';
 import { tipologyData }  from '../data/data';
 import image from "../Assets/typology-hero-section.jpg";
+import ClientTestimonials from '../components/ClientTestimonials';
+import Amenities from '../components/Amenities';
 
 const TypologyPage = () => {
 
     return (
         <div className="bg-secondary">
             <HeroSection image={image} text={"Tipologías"} />
+            <ClientTestimonials />
+            <Amenities />
             <Typologies typologies={tipologyData} />
         </div>
     );
